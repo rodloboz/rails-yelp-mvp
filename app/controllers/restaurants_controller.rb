51a290f72cb835ef0cbcfe463794fe8a5f74a6eb
Rui Freitas <dinnerpartysaboteur@gmail.com>
2017-02-15 15:04:15 +0000

    new and create actions and form for restaurants

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -6,9 +6,16 @@ class RestaurantsController < ApplicationController
   end
 
   def new
+    @restaurant = Restaurant.new
   end
 
   def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
   end
 
   def show
