d26043623821b16f22b79d940d2b97848ac33f13
Rui Freitas <dinnerpartysaboteur@gmail.com>
2017-02-15 12:42:41 +0000

    created restaurants show view

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,4 +1,5 @@
 class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: [:show]
 
   def index
     @restaurants = Restaurant.all
@@ -11,6 +12,16 @@ class RestaurantsController < ApplicationController
   end
 
   def show
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
   end
 
 end
